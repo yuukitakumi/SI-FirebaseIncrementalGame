21ddfaf6644556babe2e3f0a6be7b114784927f9
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-10-02 12:31:38 +0700

    Step1
    Step 1 tugas 8

---
+++ Step1/Assets/Scripts/LoadingController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadingController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        UserDataManager.Load();
+        SceneManager.LoadScene(1);
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
